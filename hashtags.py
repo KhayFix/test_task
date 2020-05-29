d860606d0d2ba8beeeff46a74a33c60c7d5a2dec
Serebrennikov Sergey <Smit-bb@mail.ru>
2020-05-29 22:18:15 +0500

    added code to the function for the five most common words

--- hashtags.py
+++ hashtags.py
@@ -43,7 +43,23 @@ def top_hash_tag(quantity: int = 10):
     result = [key for key, value in frequent_hash_tag]  # получаем отсортированные по убыванию популярные хэштеги
     logging.info(f"Sorted by data descending: {result}")
 
-    print(result)
+    often_used_words = {}
+    for top_hash_tags in result:
+        sorted_tweets_by_hash_tags = [re.sub(r'#[A-Za-z]+', '', text.lower())
+                                      for text in texts if top_hash_tags in text.lower()
+                                      ]
+
+        breakdown_into_words = [re.findall(r'\b[a-zA-Z]{2,15}\b', tweets)
+                                for tweets in sorted_tweets_by_hash_tags
+                                ]
+        top_words = Counter(
+            [word for tweet_words in breakdown_into_words
+             for word in tweet_words]).most_common(5)
+
+        sorted_words = [word for word, value in top_words]
+        often_used_words[top_hash_tags] = sorted_words
+
+    return sys.stdout.write(f"{str(result)}\n{str(often_used_words)}")
 
 
 if __name__ == "__main__":
